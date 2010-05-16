db785fa29a00cfac1001e8d5fe1d45b6060a74ba
Peter Marks <petermarks@Peter-Marks-MBP.local>
2010-05-16 00:46:59 -0700

    clip - pattern association.

--- src/clip.cpp
+++ src/clip.cpp
@@ -9,3 +9,26 @@ clip::clip(int _id, ofxSQLite* _sqlite) {
 string clip::getName() {
 	return name;
 }
+
+void clip::loadPatterns() {
+	
+	// select all that match pattern ids
+	ofxSQLiteSelect sel = sqlite->select("id, name")
+	.from("patterns")
+	.where("clip_id", id)
+	.execute().begin();
+	
+	// set results as instance variables
+	int count = 0;
+	while(sel.hasNext()) {
+		int pattern_id = sel.getInt();
+		string pattern_name = sel.getString();
+		
+		// store pattern in buffer
+		patterns[count] = new pattern(pattern_name, sqlite);
+		
+		// next record
+		count ++;
+		sel.next();
+	}
+}
--- src/clip.h
+++ src/clip.h
@@ -4,15 +4,20 @@
 #include <string>
 #include "ofMain.h"
 #include "ofxSQLiteHeaders.h"
+#include "pattern.h"
+
+#define NUM_PATTERNS 3
 
 class clip {
 	
 public:
 	clip(int _id, ofxSQLite* _sqlite);
 	string getName();
+	void loadPatterns();
 	
 	int id;
 	string name;
+	pattern *patterns[NUM_PATTERNS];
 	
 	ofxSQLite* sqlite;
 	
